71747a5170da5b451bb5b1456ab03bd0ccd39c97
SikhaPant <shikhapant.shinedezign@gmail.com>
2023-08-17 16:51:40 +0530

    new

--- web/shopify.js
+++ web/shopify.js
@@ -1,7 +1,7 @@
 import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
 import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
-import { restResources } from "@shopify/shopify-api/rest/admin/2023-01";
+let { restResources } = await import(`@shopify/shopify-api/rest/admin/${LATEST_API_VERSION}`);
 import dotenv from "dotenv";
 dotenv.config();
 
