d1ef8312a45258d3f0814b01bfbd3a48c54b0051
SikhaPant <shikhapant.shinedezign@gmail.com>
2023-11-27 15:06:11 +0530

    change in dashboard and bundle listing

--- web/frontend/pages/bundle.jsx
+++ web/frontend/pages/bundle.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import CreateBundle from '../components/bundle/bundle'
-import Watermark from '../components/watermark'
-import ContactUs from '../components/contactUs'
-import LogoHeader from '../components/logoHeader'
+
+import React,{Suspense,lazy} from "react";
+const CreateBundle = lazy(() => import("../components/bundle/bundle"));
+const Watermark = lazy(() => import("../components/watermark"));
+const ContactUs = lazy(() => import("../components/contactUs"));
+const LogoHeader = lazy(() => import("../components/logoHeader"));
+import { Spin } from "antd";
 const Bundle = () => {
   return (
     <div>
+<Suspense fallback={<Spin className="sd-lazy-loader"  spinning/>}>
       <LogoHeader/>
       <div className='sd-margin-top'>
       <CreateBundle/>
      <Watermark/>
      <ContactUs/>
       </div>
+</Suspense>
     </div>
   )
 }
