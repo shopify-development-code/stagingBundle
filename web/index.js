0867c76a2ce5e14b747b5f8a0f2b950b5e56093a
akashtomar01 <akash.tomar@shinedezign.com>
2024-05-08 12:19:26 +0530

    delete unused files

--- web/index.js
+++ web/index.js
@@ -4,7 +4,6 @@ import { readFileSync } from "fs";
 import express from "express";
 import serveStatic from "serve-static";
 import shopify from "./shopify.js";
-import productCreator from "./product-creator.js";
 import GDPRWebhookHandlers from "./gdpr.js";
 import cors from"cors";
 import api from "./backend/routes/api.js"
@@ -277,25 +276,6 @@ app.get("/api/privacy-policy", privacyPolicy)
 app.use("/api/*", shopify.validateAuthenticatedSession());
 app.use("/api",api)
 
-
-app.get("/api/products/count", async (_req, res) => {
-  const countData = await shopify.api.rest.Product.count({
-    session: res.locals.shopify.session,
-  });
-  res.status(200).send(countData);
-});
-app.get("/api/products/create", async (_req, res) => {
-  let status = 200;
-  let error = null;
-  try {
-    await productCreator(res.locals.shopify.session);
-  } catch (e) {
-    console.log(`Failed to process products/create: ${e.message}`);
-    status = 500;
-    error = e.message;
-  }
-  res.status(status).send({ success: status === 200, error });
-});
 app.use(serveStatic(STATIC_PATH, { index: false }));
 app.use("/*", shopify.ensureInstalledOnShop(), async (_req, res, _next) => {
   return res
