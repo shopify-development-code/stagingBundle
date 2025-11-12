7bea7c0c32496e67b97d558f0c5761e90be4e750
pallavishine <pallavi.priti@shinedezign.com>
2025-11-12 11:47:45 +0530

    credential changes

--- extensions/theme-app-extension/assets/smart.bundle.js
+++ extensions/theme-app-extension/assets/smart.bundle.js
@@ -1,6 +1,6 @@
 
 // let server = "https://reduced-stuffed-vhs-push.trycloudflare.com";
-let server = "https://bundle.shinedezigninfotech.com";
+let server = "https://bundlesgroup.com/";
 
 console.log("Smart Bundle Discount app new version 7.0");
 // let server = "https://bundle.shinedezigninfotech.com/";
